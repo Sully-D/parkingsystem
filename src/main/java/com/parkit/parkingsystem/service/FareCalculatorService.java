cf47dddc6dbbc51b4a555995dd786eb07c9caebd
Sully <sully@localhost.localdomain>
2024-01-22 13:42:49 +0100

    correct failed test and add function free less 30 minutes

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,18 +10,26 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        double duration = (double)(outHour - inHour) / (60 * 60 * 1000); // Convert millis to hours
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
+                if (duration <= 0.5){
+                    ticket.setPrice(0 * Fare.CAR_RATE_PER_HOUR);
+                    break;
+                }
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
+                if (duration <= 0.5){
+                    ticket.setPrice(0 * Fare.CAR_RATE_PER_HOUR);
+                    break;
+                }
                 ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
